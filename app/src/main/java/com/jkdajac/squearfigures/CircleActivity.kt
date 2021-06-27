9e70deb4c9f1114b03df1c20882a86f4cc4b7858
ARTEM <jkda@ukr.net>
2021-06-27 20:32:11 +0300

    added intents for activitys

--- app/src/main/java/com/jkdajac/squearfigures/CircleActivity.kt
+++ app/src/main/java/com/jkdajac/squearfigures/CircleActivity.kt
@@ -2,12 +2,27 @@ package com.jkdajac.squearfigures
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_circle.*
 
 class CircleActivity : AppCompatActivity() {
+
+    val Pi = 3.141529
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_circle)
 
+        btActCirc.setOnClickListener {
+            var rad = etActCirc.getText().toString().toInt()
+            var squear: Double = 2*Pi * (rad * rad)
+            tvActCirc.text = squear.toString()
+        }
 
     }
+
 }
+
+
+
+
--- app/src/main/java/com/jkdajac/squearfigures/MainActivity.kt
+++ app/src/main/java/com/jkdajac/squearfigures/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.jkdajac.squearfigures
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
 
+    fun onClickSquear(view : View){
+        val intent = Intent(this, SquearActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickRectangle(view : View){
+        val intent = Intent(this, RectangleActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickCircle(view : View){
+        val intent = Intent(this, CircleActivity::class.java)
+        startActivity(intent)
     }
 }
